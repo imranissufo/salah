941ba28742bee8e65f3f253f31b874a7334118a9
iissufo <imran.issufo@gmail.com>
2022-10-30 23:11:26 +0200

    on board screen

---
+++ app/src/main/java/com/salah/common/OnBoarding.java
@@ -0,0 +1,16 @@
+package com.salah.common;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.salah.R;
+
+public class OnBoarding extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_boarding);
+    }
+}
