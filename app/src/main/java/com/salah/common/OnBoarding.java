f570e6bfbd980f109b6996bf3ab0ea514d6ceae4
Imraan Momadeissufo <imraan.issufo@cedsif.gov.mz>
2022-11-01 17:36:39 +0200

    on board screen

--- app/src/main/java/com/salah/common/OnBoarding.java
+++ app/src/main/java/com/salah/common/OnBoarding.java
@@ -1,16 +1,91 @@
 package com.salah.common;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.text.Html;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
 
 import com.salah.R;
+import com.salah.util.SliderAdapter;
 
 public class OnBoarding extends AppCompatActivity {
+
+    ViewPager viewPager;
+    LinearLayout dotsLayout;
+
+    SliderAdapter sliderAdapter;
+    TextView[] dots;
+
+    Button startBtn;
+    Animation animation;
+    ViewPager.OnPageChangeListener changeListener = new ViewPager.OnPageChangeListener() {
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+            addDots(position);
+            switch (position) {
+                case 3: {
+                    animation = AnimationUtils.loadAnimation(OnBoarding.this, R.anim.bottom_anim);
+                    startBtn.setVisibility(View.VISIBLE);
+                    break;
+                }
+
+                default:
+                    startBtn.setVisibility(View.INVISIBLE);
+                    break;
+            }
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_on_boarding);
+
+        viewPager = findViewById(R.id.os_vp);
+        dotsLayout = findViewById(R.id.os_dots);
+        startBtn = findViewById(R.id.os_start_btn);
+
+        sliderAdapter = new SliderAdapter(this);
+        viewPager.setAdapter(sliderAdapter);
+
+        addDots(0);
+
+        viewPager.addOnPageChangeListener(changeListener);
+    }
+
+    private void addDots(int position) {
+        dots = new TextView[4];
+        dotsLayout.removeAllViews();
+
+        for (int i = 0; i < dots.length; i++) {
+            dots[i] = new TextView(this);
+            dots[i].setText(Html.fromHtml("&#8226;"));
+            dots[i].setTextSize(40);
+
+            dotsLayout.addView(dots[i]);
+        }
+
+        if (dots.length > 0) {
+            dots[position].setTextColor(getResources().getColor(R.color.colorPrimaryDark));
+        }
     }
 }
--- app/src/main/java/com/salah/util/SliderAdapter.java
+++ app/src/main/java/com/salah/util/SliderAdapter.java
@@ -1,4 +1,77 @@
 package com.salah.util;
 
-public class SliderAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.viewpager.widget.PagerAdapter;
+
+import com.salah.R;
+
+public class SliderAdapter extends PagerAdapter {
+
+    Context context;
+    LayoutInflater inflater;
+
+    int images[] = {
+            R.drawable.ic_salah_s1,
+            R.drawable.ic_salah_s2,
+            R.drawable.ic_salah_s3,
+            R.drawable.ic_salah_s4
+    };
+    int titles[] = {
+            R.string.os_s1_title,
+            R.string.os_s2_title,
+            R.string.os_s3_title,
+            R.string.os_s4_title
+    };
+    int descs[] = {
+            R.string.os_s1_desc,
+            R.string.os_s2_desc,
+            R.string.os_s3_desc,
+            R.string.os_s4_desc
+    };
+
+    public SliderAdapter(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return titles.length;
+    }
+
+    @Override
+    public boolean isViewFromObject(@NonNull View view, @NonNull Object object) {
+        return view == (ConstraintLayout) object;
+    }
+
+    @NonNull
+    @Override
+    public Object instantiateItem(@NonNull ViewGroup container, int position) {
+        inflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
+        View view = inflater.inflate(R.layout.slides_layout, container, false);
+
+        ImageView image = view.findViewById(R.id.sl_image);
+        TextView tile = view.findViewById(R.id.sl_title);
+        TextView desc = view.findViewById(R.id.sl_desc);
+
+        image.setImageResource(images[position]);
+        tile.setText(titles[position]);
+        desc.setText(descs[position]);
+
+        container.addView(view);
+
+        return view;
+    }
+
+    @Override
+    public void destroyItem(@NonNull ViewGroup container, int position, @NonNull Object object) {
+        container.removeView((ConstraintLayout) object);
+    }
 }
