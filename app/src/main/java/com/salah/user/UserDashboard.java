6c16816df9d02d04ddee182bccf59eb0a113aa1d
Imran Issufo <imran.issufo@gmail.com>
2022-11-04 22:37:34 +0200

    user dashboard part4

--- app/src/main/java/com/salah/user/UserDashboard.java
+++ app/src/main/java/com/salah/user/UserDashboard.java
@@ -1,6 +1,8 @@
 package com.salah.user;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.WindowManager;
@@ -9,10 +11,22 @@ import com.salah.R;
 
 public class UserDashboard extends AppCompatActivity {
 
+    RecyclerView featuredRecycler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_user_dashboard);
+
+        featuredRecycler = findViewById(R.id.ud_featured_recycler);
+
+        featuredRecycler();
+    }
+
+    private void featuredRecycler() {
+        featuredRecycler.setHasFixedSize(true);
+        featuredRecycler.setLayoutManager(new LinearLayoutManager(this,LinearLayoutManager.HORIZONTAL,false));
+
     }
 }
