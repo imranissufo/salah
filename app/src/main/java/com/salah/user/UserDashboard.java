1c2997523c6d2f0cfc6993a042c31fa85b54eb62
Imraan Momadeissufo <imraan.issufo@cedsif.gov.mz>
2022-11-08 17:24:35 +0200

    City Guide App – Part 4.2

--- app/src/main/java/com/salah/user/UserDashboard.java
+++ app/src/main/java/com/salah/user/UserDashboard.java
@@ -2,12 +2,20 @@ package com.salah.user;
 
 import android.graphics.drawable.GradientDrawable;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.navigation.NavigationView;
 import com.salah.R;
 import com.salah.model.Location;
 import com.salah.util.CategoryAdapter;
@@ -16,10 +24,16 @@ import com.salah.util.MostViewedAdapter;
 
 import java.util.ArrayList;
 
-public class UserDashboard extends AppCompatActivity {
+public class UserDashboard extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    static final float END_SCALE = 0.7f;
 
     RecyclerView featuredRecycler, mostViewedRecycler, categoryRecycler;
     RecyclerView.Adapter adapter;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    ImageView menuIcon;
+    LinearLayout contentView;
     private GradientDrawable gradient1, gradient2, gradient3, gradient4;
 
     @Override
@@ -32,9 +46,54 @@ public class UserDashboard extends AppCompatActivity {
         mostViewedRecycler = findViewById(R.id.ud_most_recycler);
         categoryRecycler = findViewById(R.id.ud_category_recycler);
 
+        //Menu Hooks
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigation_view);
+        menuIcon = findViewById(R.id.ud_menu);
+        contentView = findViewById(R.id.content);
+
+        naviagtionDrawer();
+
         featuredRecycler();
         mostViewedRecycler();
         categoriesRecycler();
+    }
+
+    private void naviagtionDrawer() {
+        //Naviagtion Drawer
+        navigationView.bringToFront();
+        navigationView.setNavigationItemSelectedListener(this);
+        navigationView.setCheckedItem(R.id.nav_home);
+        menuIcon.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (drawerLayout.isDrawerVisible(GravityCompat.START))
+                    drawerLayout.closeDrawer(GravityCompat.START);
+                else drawerLayout.openDrawer(GravityCompat.START);
+            }
+        });
+        animateNavigationDrawer();
+    }
+
+    private void animateNavigationDrawer() {
+        //Add any color or remove it to use the default one!
+        //To make it transparent use Color.Transparent in side setScrimColor();
+        //drawerLayout.setScrimColor(Color.TRANSPARENT);
+        drawerLayout.addDrawerListener(new DrawerLayout.SimpleDrawerListener() {
+            @Override
+            public void onDrawerSlide(View drawerView, float slideOffset) {
+                // Scale the View based on current slide offset
+                final float diffScaledOffset = slideOffset * (1 - END_SCALE);
+                final float offsetScale = 1 - diffScaledOffset;
+                contentView.setScaleX(offsetScale);
+                contentView.setScaleY(offsetScale);
+                // Translate the View, accounting for the scaled width
+                final float xOffset = drawerView.getWidth() * slideOffset;
+                final float xOffsetDiff = contentView.getWidth() * diffScaledOffset / 2;
+                final float xTranslation = xOffset - xOffsetDiff;
+                contentView.setTranslationX(xTranslation);
+            }
+        });
     }
 
     private void categoriesRecycler() {
@@ -92,5 +151,16 @@ public class UserDashboard extends AppCompatActivity {
         mostViewedRecycler.setAdapter(adapter);
     }
 
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return true;
+    }
 
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerVisible(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
