c533f817a8480cff10e85e25e4c6ae3cb1e5ecb1
Imran Issufo <imran.issufo@gmail.com>
2022-11-08 22:03:03 +0200

    City Guide App – Part 6

---
+++ app/src/main/java/com/salah/user/Categories.java
@@ -0,0 +1,16 @@
+package com.salah.user;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.salah.R;
+
+public class Categories extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_categories);
+    }
+}
