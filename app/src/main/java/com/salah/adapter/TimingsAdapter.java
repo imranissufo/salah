56a9391128164e561176e0afc39b7bf927bb3e95
Imraan Momadeissufo <imraan.issufo@cedsif.gov.mz>
2022-11-17 12:39:16 +0200

    Salah App – 2022.11.17 12:30 Dashboard

--- app/src/main/java/com/salah/adapter/TimingsAdapter.java
+++ app/src/main/java/com/salah/adapter/TimingsAdapter.java
@@ -3,7 +3,6 @@ package com.salah.adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -12,10 +11,7 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.salah.R;
-import com.salah.model.Location;
 import com.salah.model.Timings;
-
-import java.util.ArrayList;
 
 public class TimingsAdapter extends FirebaseRecyclerAdapter<Timings, TimingsAdapter.TimingsViewHolder> {
 
@@ -38,26 +34,54 @@ public class TimingsAdapter extends FirebaseRecyclerAdapter<Timings, TimingsAdap
     @NonNull
     @Override
     public TimingsViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view  = LayoutInflater.from(parent.getContext()).inflate(R.layout.timings_card, parent,false);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.timings_card, parent, false);
         TimingsViewHolder viewHolder = new TimingsViewHolder(view);
         return viewHolder;
     }
 
     @Override
     protected void onBindViewHolder(@NonNull TimingsViewHolder holder, int position, @NonNull Timings model) {
-        holder.desc.setText(model.getCode());
+        holder.code.setText(model.getCode());
+        holder.suhur.setText(model.getSuhur());
+        holder.fajr.setText(model.getFajr());
+        holder.sunrise.setText(model.getSunrise());
+        holder.zawal.setText(model.getZawal());
+        holder.zuhr.setText(model.getZuhr());
+        holder.assr.setText(model.getAssr());
+        holder.sunset.setText(model.getSunset());
+        holder.magrib.setText(model.getMagrib());
+        holder.isha.setText(model.getIsha());
     }
 
-    public static class TimingsViewHolder extends RecyclerView.ViewHolder{
+    public static class TimingsViewHolder extends RecyclerView.ViewHolder {
 
-        TextView title;
-        TextView desc;
+        TextView code;
+        TextView month;
+        TextView day;
+        TextView suhur;
+        TextView fajr;
+        TextView sunrise;
+        TextView zawal;
+        TextView zuhr;
+        TextView assr;
+        TextView sunset;
+        TextView magrib;
+        TextView isha;
+        TextView location;
 
         public TimingsViewHolder(@NonNull View itemView) {
             super(itemView);
 
-            title = itemView.findViewById(R.id.tm_date_lb);
-            desc = itemView.findViewById(R.id.tm_date_vl);
+            code = itemView.findViewById(R.id.tm_date_vl);
+            suhur = itemView.findViewById(R.id.tm_suhur_vl);
+            fajr = itemView.findViewById(R.id.tm_fajr_vl);
+            sunrise = itemView.findViewById(R.id.tm_sunrise_vl);
+            zawal = itemView.findViewById(R.id.tm_zawal_vl);
+            zuhr = itemView.findViewById(R.id.tm_zuhr_vl);
+            assr = itemView.findViewById(R.id.tm_assr_vl);
+            sunset = itemView.findViewById(R.id.tm_sunset_vl);
+            magrib = itemView.findViewById(R.id.tm_magrib_vl);
+            isha = itemView.findViewById(R.id.tm_isha_vl);
 
         }
     }
